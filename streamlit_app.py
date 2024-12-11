c4873df0083a6623d7caafde0440ab50d0e03773
João Sampaio <20240748@novaims.unl.pt>
2024-12-11 20:36:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,18 +16,18 @@ except Exception as e:
     st.error(f"Error loading pickle file: {e}")
 
 st.header('Input your data here',divider="red")
+with st.expander:
+    Accident_Date = st.date_input("Accident Date", datetime.date(2024, 12, 11))
 
-Accident_Date = st.date_input("Accident Date", datetime.date(2024, 12, 11))
+    Age_at_njury = st.slider("Insert your value", 0, 117, 42)
 
-Age_at_njury = st.slider("Insert your value", 0, 117, 42)
+    Alternative_Dispute_Resolution =st.selectbox("How would you like to be contacted?",("Y", "N ", "U"))
 
-Alternative_Dispute_Resolution =st.selectbox("How would you like to be contacted?",("Y", "N ", "U"))
+    Average_Weekly_Wage = st.slider("Insert your value", 0, 2828079, 491.0892)
 
-Average_Weekly_Wage = st.slider("Insert your value", 0, 2828079, 491.0892)
+    Birth_Year = st.slider("Insert your value", 0, 2018, 1977)
 
-Birth_Year = st.slider("Insert your value", 0, 2018, 1977)
-
-IME_4_Count = st.slider("Insert your value", 1, 73, 2)
+    IME_4_Count = st.slider("Insert your value", 1, 73, 2)
 
 
     
