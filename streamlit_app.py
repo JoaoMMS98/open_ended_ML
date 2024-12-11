17d72252824ecfc93f97f4671813ebda7efdb387
João Sampaio <20240748@novaims.unl.pt>
2024-12-11 18:17:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ with st.expander:
     except Exception as e:
         st.error(f"Error loading pickle file: {e}")
     
-    Accident Date = st.date_input("Accident Date", datetime.date(2024, 12, 11))
+    Accident_Date = st.date_input("Accident Date", datetime.date(2024, 12, 11))
     
     
 
