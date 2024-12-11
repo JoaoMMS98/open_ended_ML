1691fca765829b81bc4e3e5c39a6b9c2c979298e
João Sampaio <20240748@novaims.unl.pt>
2024-12-11 19:31:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,14 +16,19 @@ except Exception as e:
     st.error(f"Error loading pickle file: {e}")
 
 st.header('Input your data here',divider="red")
-st.subheader("These subheaders have rotating dividers", divider="red")
-Accident_Date = st.date_input("Insert your value", datetime.date(2024, 12, 11))
+
 st.subheader("These subheaders have rotating dividers", divider="red")
 Age_at_njury = st.slider("Insert your value", 0, 117, 42)
+
+st.subheader("These subheaders have rotating dividers", divider="red")
+Accident_Date = st.date_input("Insert your value", datetime.date(2024, 12, 11))
+
 st.subheader("These subheaders have rotating dividers", divider="red")
 Average_Weekly_Wage = st.slider("Insert your value", 0, 2828079, 491.0892)
+
 st.subheader("These subheaders have rotating dividers", divider="red")
 Birth_Year = st.slider("Insert your value", 0, 2018, 1977)
+
 st.subheader("IME-4 Count", divider="red")
 IME_4_Count = st.slider("Insert your value", 1, 73, 2)
 
